feb6e73f5f0f82eba970b7e88823f1da6da529fe
geoffrey-diederichs <geoffrey.diederichs@ynov.com>
2023-05-23 15:48:52 +0200

    05_Loop

---
+++ 05_Loop/README.md
@@ -0,0 +1,16 @@
+# Loop
+
+```
+$ nasm -felf64 loop.asm && ld loop.o -o loop
+$ ./loop
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+```
---
+++ 05_Loop/loop.asm
@@ -0,0 +1,51 @@
+; Printing given string
+%macro print 2
+	mov rax, 1
+	mov rdi, 1
+	lea rsi, %1
+	lea rdx, %2
+	syscall
+%endmacro
+
+; Storing user input in given variable
+%macro user_input 2
+	mov rax, 0
+	mov rdi, 1
+	mov rsi, %1
+	mov rdx, %2
+	syscall
+%endmacro
+
+section .bss
+	
+section .data
+	ite db "0"
+	ite_len equ $-ite	
+	newline db 10	
+
+section .text
+	global _start
+
+_start:
+	; Printing ite
+	print ite, ite_len
+	print newline, 1
+	
+	; Moving ite into rax and increasing it by 1
+	mov al, [ite]
+	sub al, "0"
+	inc al
+	
+	; If rax over 9, exit
+	cmp al, 9
+	jg _exit	
+	
+	; If not, moves it back to ite and repeat
+	add al, "0"
+	mov [ite], al	
+	jmp _start
+
+_exit:
+	mov rax, 60
+	mov rdi, 1 
+	syscall	
